66a46b3eef08b59225f1df507a5ebce54ad3a87b
mattiasminejev <mattiasminejev@gmail.com>
2024-12-15 17:31:12 +0200

    muutus

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
+app.use(cors({ origin: 'http://localhost:8081', credentials: true }));
 // We need to include "credentials: true" to allow cookies to be represented  
 // Also "credentials: 'include'" need to be added in Fetch API in the Vue.js App
 
@@ -28,7 +28,7 @@ app.listen(port, () => {
     console.log("Server is listening to port " + port)
 });
 
-/*
+
 // is used to check whether a user is authinticated
 app.get('/auth/authenticate', async(req, res) => {
     console.log('authentication request has been arrived');
@@ -59,12 +59,32 @@ app.get('/auth/authenticate', async(req, res) => {
     }
 });
 
+app.post('/auth/check-email', async (req, res) => {
+    try {
+      const { email } = req.body;
+      const existingUser = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+      if (existingUser.rows.length > 0) {
+        return res.json({ exists: true });
+      }
+      res.json({ exists: false });
+    } catch (err) {
+      console.error(err.message);
+      res.status(400).send(err.message);
+    }
+  });
+  
+
 // signup a user
 app.post('/auth/signup', async(req, res) => {
     try {
         console.log("a signup request has arrived");
         console.log(req.body);
         const { email, password } = req.body;
+
+        const existingUser = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+        if (existingUser.rows.length > 0) {
+            return res.status(400).json({ error: "Email is already registered" });
+        }
 
         const salt = await bcrypt.genSalt(); //  generates the salt, i.e., a random string
         const bcryptPassword = await bcrypt.hash(password, salt) // hash the password and the salt 
@@ -80,7 +100,7 @@ app.post('/auth/signup', async(req, res) => {
             .status(201)
             .cookie('jwt', token, { maxAge: 6000000, httpOnly: true })
             .json({ user_id: authUser.rows[0].id })
-            .send;
+            .send();
     } catch (err) {
         console.error(err.message);
         res.status(400).send(err.message);
@@ -103,7 +123,7 @@ app.post('/auth/login', async(req, res) => {
         bcrypt.compare method takes the first argument as a plain text and the second argument as a hash password. 
         If both are equal then it returns true else returns false.
         */
-/*
+
         //Checking if the password is correct
         const validPassword = await bcrypt.compare(password, user.rows[0].password);
         //console.log("validPassword:" + validPassword);
@@ -125,92 +145,3 @@ app.get('/auth/logout', (req, res) => {
     console.log('delete jwt request arrived');
     res.status(202).clearCookie('jwt').json({ "Msg": "cookie cleared" }).send
 });
-*/
-
-// add post
-app.post('/api/posts', async(req, res) => {
-    try {
-        console.log("a post request has arrived");
-        const post = req.body;
-        const currentTime = new Date();
-        const newpost = await pool.query(
-            "INSERT INTO posts(content, created_at) values ($1, $2)    RETURNING*", [post.content, currentTime]
-            // $1, $2, $3 are mapped to the first, second and third element of the passed array (post.title, post.body, post.urllink)
-            // The RETURNING keyword in PostgreSQL allows returning a value from the insert or update statement.
-            // using "*" after the RETURNING keyword in PostgreSQL, will return everything
-        );
-        res.json(newpost);
-    } catch (err) {
-        console.error(err.message);
-    }
-}); 
-
-
-
-// get all posts
-app.get('/api/posts', async(req, res) => {
-    try {
-        console.log("get posts request has arrived");
-        const posts = await pool.query(
-            "SELECT * FROM posts"
-        );
-        res.json(posts.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
-});
-
-
-// get specific post
-app.get('/api/posts/:id', async(req, res) => {
-    try {
-        console.log("get a post with route parameter  request has arrived");
-        // The req.params property is an object containing properties mapped to the named route "parameters". 
-        // For example, if you have the route /posts/:id, then the "id" property is available as req.params.id.
-        const { id } = req.params; // assigning all route "parameters" to the id "object"
-        const posts = await pool.query( // pool.query runs a single query on the database.
-            //$1 is mapped to the first element of { id } (which is just the value of id). 
-            "SELECT * FROM posts WHERE id = $1", [id]
-        );
-        res.json(posts.rows[0]); // we already know that the row array contains a single element, and here we are trying to access it
-        // The res.json() function sends a JSON response. 
-        // This method sends a response (with the correct content-type) that is the parameter converted to a JSON string using the JSON.stringify() method.
-    } catch (err) {
-        console.error(err.message);
-    }
-}); 
-
-
-
-// update a post
-app.put('/api/posts/:id', async(req, res) => {
-    try {
-        const currentTime = new Date();
-        const { id } = req.params;
-        const post = req.body;
-        console.log("update request has arrived");
-        const updatepost = await pool.query(
-            "UPDATE posts SET (content, created_at) = ($2, $3) WHERE id = $1", [id, post.content, currentTime]
-        );
-        res.json(updatepost);
-    } catch (err) {
-        console.error(err.message);
-    }
-});
-
-
-// delete specific post
-app.delete('/api/posts/:id', async(req, res) => {
-    try {
-        const { id } = req.params;
-        //const post = req.body; // we do not need a body for a delete request
-        console.log("delete a post request has arrived");
-        const deletepost = await pool.query(
-            "DELETE FROM posts WHERE id = $1", [id]
-        );
-        res.json(deletepost);
-    } catch (err) {
-        console.error(err.message);
-    }
-}); 
-
